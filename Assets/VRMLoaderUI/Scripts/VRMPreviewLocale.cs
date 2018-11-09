7bc6f7690f52f86922e1a8dd058dfeefd1f01cab
m2wasabi <m2wasabi@gmail.com>
2018-11-09 23:16:32 +0900

    Add VRMPreviewLocale

---
+++ Assets/VRMLoaderUI/Scripts/VRMPreviewLocale.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRMLoader
+{
+    public class VRMPreviewLocale : MonoBehaviour {
+        void Start () {
+            
+        }
+    }
+}
